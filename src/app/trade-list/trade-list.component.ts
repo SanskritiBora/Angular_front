4837396a5a6c0f0af2c43d340ab4de30deeba77d
Anmol Singh Suri <anmolss.111@gmail.com>
2019-03-25 22:58:09 +0530

    Working

--- src/app/trade-list/trade-list.component.ts
+++ src/app/trade-list/trade-list.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './trade';
-import { TradeService } from './trade.service';
+import { Trade } from '../trade';
+// import { TradeService } from '../trade.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
 @Component({
-  selector: 'app-trade-list',
-  templateUrl: './trade-list.component.html',
-   providers: [ TradeService ]
-  styleUrls: ['./trade-list.component.css']
+	selector: 'app-trade-list',
+	templateUrl: './trade-list.component.html',
+	styleUrls: ['./trade-list.component.css']
 })
 export class TradeListComponent implements OnInit {
 trades : Trade[];
-  constructor(private tradeService: TradeService) { }
+	constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.getTrade();
-  }
-  getTrades(): void {
-      this.tradeService.getTrade()
-        .subscribe(trades => this.trades = trades);
-    }
+	ngOnInit() {
+		this.getTrades();
+	}
+	getTrades(): void {
+			this.http.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo")
+			.subscribe(data =>
+				console.log(data)
+			);
+		}
 }
--- src/app/trade.service.ts
+++ src/app/trade.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Trade } from './trade';
-@Injectable({
-  providedIn: 'root'
-})
-export class TradeService {
-url="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=BU2N9M0SYP8Z2DM7"
-  constructor( private http: HttpClient) { }
-
-  /** GET heroes from the server */
- getTrade(): Observable<Trade[]> {
-   return this.http.get<Trade[]>(this.url);
-}
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+// import { Trade } from './trade';
+// @Injectable({
+// 	providedIn: 'root'
+// })
+// export class TradeService {
+// url="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+// 	constructor( private http: HttpClient) { }
+//
+// 	/** GET heroes from the server */
+// 	getTrades(): Observable<any> {
+// 		return this.http.get(this.url)
+// 				.map((response: Response) =>
+// 				 response.json());
+// 	}
+// }
